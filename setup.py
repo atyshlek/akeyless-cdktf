42796cf83da4b6d87cf70fed688caec91579b014
Alex Tyshlek <atyshlek@dropbox.com>
2024-01-22 17:47:12 -0600

    refactor

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="cdktfakeyless",
+    name="akeyless_cdktf",
     version="1.4.1",
     packages=find_packages(),
-    install_requires=["cdktf>=0.14.3"],
+    install_requires=["cdktf>=0.20.0"],
 )
