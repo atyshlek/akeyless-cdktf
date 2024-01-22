9c354bd151be18a238f9fbe7ebfc458ccb877276
Alex Tyshlek <atyshlek@dropbox.com>
2024-01-22 16:50:19 -0600

    add source

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="akeyless_cdktf",
     version="1.4.1",
-    packages=find_packages(""),
+    packages=find_packages("./akeyless"),
     install_requires=["cdktf>=0.14.3"],
 )
