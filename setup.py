9adb2cf1ae3d56bc31d6c5ef0aa29ff758c8ff2d
Alex Tyshlek <atyshlek@dropbox.com>
2024-01-22 17:20:49 -0600

    name fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="akeyless-cdktf",
+    name="akeyless_cdktf",
     version="1.4.1",
     packages=find_packages(),
     install_requires=["cdktf>=0.14.3"],
